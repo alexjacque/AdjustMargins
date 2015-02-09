79788da7e1301b4b65174242377dfe79c967ac78
Alex Jacque <ajacque@gmail.com>
2015-02-08 22:58:35 -0500

    initial commit

---
+++ AdjustMargins.roboFontExt/lib/AdjustMargins.py
@@ -0,0 +1,148 @@
+# The MIT License (MIT)
+
+# Copyright (c) 2015 Alex Jacque <alexjacque.com>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# import our magic makers
+from AppKit import *
+import vanilla
+import math
+from defconAppKit.windows.baseWindow import BaseWindowController
+
+class AdjustMargins(BaseWindowController):
+    
+    # setup our interface
+    def __init__(self): 
+        windowWidth = 220
+        windowHeight = 270
+        
+        # amount area
+        self.w = vanilla.FloatingWindow((windowWidth,windowHeight),"AdjustMargins")
+        self.w.amountText = vanilla.TextBox((15,17,65,22),"Amount:",sizeStyle="small")
+        self.w.amountTextBox = vanilla.EditText((70,12,-15,22))
+        
+        # divider
+        self.w.divider1 = vanilla.HorizontalLine((15,48,-15,1))
+        
+        # glyphs scope area
+        self.w.iconAllGlyphs = vanilla.ImageView((15,65,12,13))
+        self.w.iconAllGlyphs.setImage(imagePath="allGlyphs.png")
+        self.w.iconSelGlyphs = vanilla.ImageView((15,91,12,13))
+        self.w.iconSelGlyphs.setImage(imagePath="selectedGlyphs.png")
+        self.w.glyphsRadioGroup = vanilla.RadioGroup((32,60,-15,50),["All Glyphs","Selected Glyphs"],sizeStyle="small")
+        self.w.glyphsRadioGroup.set(1) # default to just selected glyphs
+        
+        # divider
+        self.w.divider2 = vanilla.HorizontalLine((15,121,-15,1))
+        
+        # margins area
+        self.w.iconBothMargins = vanilla.ImageView((15,137,12,12))
+        self.w.iconBothMargins.setImage(imagePath="bothMargins.png")
+        self.w.iconLeftMargin = vanilla.ImageView((15,161,12,12))
+        self.w.iconLeftMargin.setImage(imagePath="leftMargin.png")
+        self.w.iconRightMargin = vanilla.ImageView((15,185,12,12))
+        self.w.iconRightMargin.setImage(imagePath="rightMargin.png")
+        self.w.marginSelectionGroup = vanilla.RadioGroup((32,133,-15,70),["Both Margins","Left Margin Only","Right Margin Only"],sizeStyle="small")
+        self.w.marginSelectionGroup.set(0) # default both margins
+        
+        # divider
+        self.w.divider3 = vanilla.HorizontalLine((15,216,-15,1))
+        
+        # commit button
+        self.w.commitButton = vanilla.Button((15, 232, -15, 20), "Commit Adjustments", sizeStyle="small", callback=self.commitButtonCallback)
+        
+        self.w.open() # go go gadget window
+    
+    
+    def commitButtonCallback(self, sender):
+        
+        if CurrentFont() is None:  
+            self.showMessage("Nothing Open","Please open a file first.") # show error, can't work with nothing...
+        else:        
+            font = CurrentFont()
+            
+            adjustmentAmount = self.w.amountTextBox.get()
+            
+            if len(adjustmentAmount) == 0:
+                self.showMessage("How Much?","Please enter an adjustment amount.") # show error, can't work with nothing...
+            else:
+                
+                # which glyphs we're working with
+                if self.w.glyphsRadioGroup.get() == 0:
+                    glyphs = font.keys() # all
+                else:
+                    glyphs = font.selection # a subset
+                    if glyphs == []:
+                        # "selected glyphs" is checked but nothing is selected
+                        self.showMessage("Adjust What?", "Please select at least one glyph to preform the adjustments on.")
+                        return # bail
+                    
+                
+                # if the user want to use an existing margin
+                if adjustmentAmount[:1] == "=":
+                    characterName = adjustmentAmount[1:] # everything after the =
+                
+                    # see if the glyph even exists at all
+                    if characterName in font:
+                        character = font[characterName]
+                        adjustmentLeft = int(character.leftMargin) # integers only please
+                        adjustmentRight = int(character.rightMargin) # integers only please
+                    else:
+                        self.showMessage("Glyph Not Found.","Glyph \"" + characterName + "\" does not exist in this font.");
+                
+                
+                    # preform adjustments using the supplied character's metrics
+                    for glyphName in glyphs:
+                        glyph = font[glyphName]
+                        glyph.prepareUndo("Metric Adjustment")
+                        if self.w.marginSelectionGroup.get() == 0: # adjust both margins
+                            glyph.leftMargin = adjustmentLeft
+                            glyph.rightMargin = adjustmentRight
+                            print("Adjusted left margin to " + str(adjustmentLeft) + " for glyph: " + glyphName)
+                            print("Adjusted right margin to " + str(adjustmentRight) + " for glyph: " + glyphName)
+                        elif self.w.marginSelectionGroup.get() == 1: # adjust left margin only
+                            glyph.leftMargin = adjustmentLeft
+                            print("Adjusted left margin to " + str(adjustmentLeft) + " for glyph: " + glyphName)
+                        elif self.w.marginSelectionGroup.get() == 2: # adjust right margin only
+                            glyph.rightMargin = adjustmentRight
+                            print("Adjusted right margin to " + str(adjustmentRight) + " for glyph: " + glyphName)
+                        glyph.performUndo()
+                        
+                
+                else: # for all other adjustments (+ or -)
+                    adjustmentAmount = int(adjustmentAmount) # make sure we're dealing with integers
+                
+                    for glyphName in glyphs:
+                        glyph = font[glyphName]
+                        glyph.prepareUndo("Metric Adjustment")
+                        if self.w.marginSelectionGroup.get() == 0: # adjust both margins
+                            glyph.leftMargin = glyph.leftMargin + adjustmentAmount
+                            glyph.rightMargin = glyph.rightMargin + adjustmentAmount
+                            print("Adjusted left and right margins " + str(adjustmentAmount) + " for glyph: " + glyphName)
+                        elif self.w.marginSelectionGroup.get() == 1:
+                            glyph.leftMargin = glyph.leftMargin + int(adjustmentAmount)
+                            print("Adjusted left margin " + str(adjustmentAmount) + " for glyph: " + glyphName)
+                        elif self.w.marginSelectionGroup.get() == 2: # adjust right margin only
+                            glyph.rightMargin = glyph.rightMargin + adjustmentAmount
+                            print("Adjusted right margin " + str(adjustmentAmount) + " for glyph: " + glyphName)
+                        glyph.performUndo()
+                                
+                
+OpenWindow(AdjustMargins)
---
+++ README.md
@@ -0,0 +1,40 @@
+# AdjustMargins
+AdjustMargins is a RoboFont extension created in 2015 by [Alex Jacque](http://alexjacque.com).
+
+![AdjustMargins Interface](https://dl.dropboxusercontent.com/u/32129742/dont-delete/github/AdjustMargins/AdjustMargins_interface.png)
+
+
+## What does it do?
+AdjustMargins batch adjusts the left and right margins on your glyphs.
+
+
+### Amount:
+The amount you with to add/subtract from the margins on your selected glyph(s) (eg. 100, -20, 42, -5). You can also use the = operand followed by a glyph name to have the margins of your selected glyphs(s) to be changed to that glyphs margin measurements (eg. =A, =Oacute, =uni20B9).
+
+### Glyph Range:
+Change all of the glyphs in your file, or just a selected subset.
+
+### Which Margins:
+Change both margins, change just the left margins, or change just the right margins
+
+
+## The MIT License (MIT)
+Copyright (c) 2015 Alex Jacque ([alexjacque.com](http://alexjacque.com))
+
+Permission is hereby granted, free of charge, to any person obtaining a copy 
+of this software and associated documentation files (the "Software"), to deal 
+in the Software without restriction, including without limitation the rights 
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in 
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+THE SOFTWARE.
